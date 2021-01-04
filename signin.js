6eda3fbe6dd1a6b3ee20c7d78f216d7d59936a4c
hanseulhee <3021062@gmail.com>
2021-01-04 16:40:38 +0900

    signin page

--- signin.js
+++ signin.js
@@ -1,8 +1,6 @@
 const form = document.querySelector(".form");
 const name = document.querySelector(".name");
-const email = document.querySelector(".email");
 const password = document.querySelector(".password");
-const password2 = document.querySelector(".password2");
 
 
 
@@ -10,25 +8,14 @@ form.addEventListener("submit", function (e) {
   e.preventDefault();
   inputpassword(password);
   inputname();
-  inputemail(email);
-  inputpassword2(password, password2);
 });
+
 const setDefault = (input) => {
   const formControl = input.parentElement;
   formControl.className = "form-control";
 };
 
-const inputpassword2 = () => {
-  const input1 = password.value;
-  const input2 = password2.value;
-  if (password2.value.length === 0) {
-    errormessage(password2, `Password2 is required`);
-  } else if (input1 === input2) {
-    success(password2);
-  } else {
-    errormessage(password2, `Passwords do not match`);
-  }
-};
+
 
 const inputpassword = () => {
   if (password.value.length >= 6) {
@@ -38,14 +25,6 @@ const inputpassword = () => {
   }
 };
 
-const inputemail = () => {
-  const rule = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (rule.test(email.value.trim())) {
-    success(email);
-  } else {
-    errormessage(email, `Email is not valid`);
-  }
-};
 
 const inputname = () => {
   if (name.value.length === 0) {
@@ -71,9 +50,7 @@ const errormessage = (input, message) => {
 
 function init() {
   name.addEventListener("input", inputname);
-  email.addEventListener("input", inputemail);
   password.addEventListener("input", inputpassword);
-  password2.addEventListener("input", inputpassword2);
 }
 
 init();
